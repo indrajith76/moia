4ec64ef93a2c68fcb5274c463f7548109b126e1e
indrajith76 <indrajithgoswami@gmail.com>
2024-08-10 00:43:58 +0600

    added responsiveness

--- app/page.js
+++ app/page.js
@@ -4,20 +4,20 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="home-bg relative h-[1024px]">
+    <div className="home-bg relative h-screen md:h-[1024px] overflow-hidden">
       <>
         <img
-          className="absolute -top-[148px] left-5 z-0"
+          className="absolute -top-10 md:-top-[148px] left-5 z-0"
           src="/images/moia-bg.png"
           alt=""
         />
         <img
-          className="absolute top-[179px] left-5 z-0"
+          className="absolute top-48 md:top-[179px] left-5 z-0"
           src="/images/moia-bg.png"
           alt=""
         />
         <img
-          className="absolute top-[506px] left-5 z-0"
+          className="absolute top-[460px] md:top-[506px] left-5 z-0"
           src="/images/moia-bg2.png"
           alt=""
         />
@@ -44,8 +44,8 @@ export default function Home() {
       </>
       <Navbar />
 
-      <div className="absolute top-28 lg:top-[190px] left-10 lg:left-[300px]">
-        <p className="text-white text-[48px] leading-[50px]">
+      <div className="absolute top-20 md:top-28 lg:top-[190px] left-5 md:left-10 lg:left-[300px]">
+        <p className="text-white text-2xl md:text-[48px] leading-8 md:leading-[50px]">
           collective
           <br />
           of onchain <br />
@@ -53,20 +53,20 @@ export default function Home() {
           and <span className="text-[#EAE7B1]">market makers.</span>
         </p>
         <img
-          className="absolute top-6 right-[100px]"
+          className="absolute top-2 md:top-6 right-10 md:right-[100px]"
           src="/images/Star-6.png"
           alt=""
         />
       </div>
 
-      <p className="text-[21px] text-white absolute left-[33px] top-[480px] lg:top-[520px]">
+      <p className="text-[21px] text-white absolute left-[33px] top-[430px] md:top-[480px] lg:top-[520px]">
         moia capital
         <br />
         copyright©2024
       </p>
 
-      <div className=" text-white absolute top-[370px] lg:top-[415px] right-6 w-[325px]">
-        <div className="flex items-center gap-4">
+      <div className=" text-white absolute top-[240px] md:top-[360px] lg:top-[415px] right-6 w-[325px]">
+        <div className="flex items-center gap-4 mb-4 lg:mb-8">
           <h3 className="text-4xl lg:text-[62px] font-medium">+$150K</h3>
           <p className="text-[13px] leading-[14px]">(TOP TRADER 30d PNL)</p>
         </div>
@@ -84,17 +84,17 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="bg-[#EAE7B1] p-6 absolute bottom-0 h-[164px] w-full flex items-center justify-between">
+      <div className="bg-[#EAE7B1] px-3 py-5 md:p-6 absolute bottom-0 md:h-[164px] w-full flex flex-col md:flex-row items-center justify-between gap-2 md:gap-0">
         <div className="flex items-center gap-6">
-          <img src="/images/LOGO-2.png" alt="" />
+          <img className="w-[40px] md:w-auto" src="/images/LOGO-2.png" alt="" />
           <img
-            className="w-[300px] lg:w-full"
+            className="w-[280px] md:w-[300px] lg:w-full"
             src="/images/bottom-text.png"
             alt=""
           />
         </div>
         <Link href={""}>
-          <button className="flex items-center gap-2 bg-white px-5 lg:px-[70px] py-5 rounded-[36px]">
+          <button className="flex items-center gap-2 bg-white px-5 lg:px-[70px] py-2 md:py-5 rounded-[36px]">
             apply in discord <img src="/images/link-icon.png" alt="" />
           </button>
         </Link>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,30 +2,30 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="p-5 absolute z-30">
-      <ul className="flex items-center gap-[67px]">
+    <div className="p-2 md:p-5 absolute z-30">
+      <ul className="flex items-center gap-3  md:gap-[67px]">
         <li>
           <Link href="#">
-            <img src="/images/LOGO.png" alt="" />
+            <img className="w-7 md:w-auto" src="/images/LOGO.png" alt="" />
           </Link>
         </li>
         <li>
-          <Link className="text text-base text-white" href="#">
+          <Link className="text-[13px] md:text-base text-white" href="#">
             apply
           </Link>
         </li>
         <li>
-          <Link className="text text-base text-white" href="#">
+          <Link className="text-[13px] md:text-base text-white" href="#">
             twitter
           </Link>
         </li>
         <li>
-          <Link className="text text-base text-white" href="#">
+          <Link className="text-[13px] md:text-base text-white" href="#">
             discord
           </Link>
         </li>
         <li>
-          <Link className="text text-base text-white" href="#">
+          <Link className="text-[13px] md:text-base text-white" href="#">
             telegram
           </Link>
         </li>
